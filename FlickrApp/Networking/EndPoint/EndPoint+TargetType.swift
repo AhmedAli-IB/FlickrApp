f293b32d4eedf262b1f0ee3cf66f3e727e2117b8
Ahmed Ali <ahmedali@Ahmeds-MacBook-Pro-2.local>
2021-01-10 13:23:28 +0200

    Feat network manager and newtwork error
    Add network manager with generic request method and add network error to handle network error.

---
+++ FlickrApp/Networking/EndPoint/EndPoint+TargetType.swift
@@ -0,0 +1,56 @@
+//
+//  EndPoint+TargetType.swift
+//  FlickrApp
+//
+//  Created by Ahmed Ali on 10/01/2021.
+//
+
+import Foundation
+import Moya
+
+// MARK: - EndPoint + TargetType
+//
+extension AppEndPoint: TargetType {
+    
+    var baseURL: URL {
+        return BaseURL.url
+    }
+    
+    var path: String {
+        switch self {
+        
+        // MARK: - Home
+        
+        case .getPhotos:
+            return "services/rest"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        default:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        
+        // MARK: - Home
+        
+        case .getPhotos(let request):
+            let parameters = try? request.asDictionary()
+            return .requestParameters(parameters: parameters ?? [:], encoding: URLEncoding.default)
+            
+        }
+    }
+    
+    var headers: [String: String]? {
+        return nil
+    }
+    
+}
---
+++ FlickrApp/Networking/Extensions/Encodable+Dictionary.swift
@@ -0,0 +1,22 @@
+//
+//  Encodable+Dictionary.swift
+//  FlickrApp
+//
+//  Created by Ahmed Ali on 10/01/2021.
+//
+
+import Foundation
+
+extension Encodable {
+    
+    /// convert data to dictionary
+    /// - Returns: dictionary
+    ///
+    func asDictionary() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        guard let dic = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            throw NSError()
+        }
+        return dic
+    }
+}
