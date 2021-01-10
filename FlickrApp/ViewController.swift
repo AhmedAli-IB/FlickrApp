e2b119027ac209c019b5aa1791ebce376cb27d6a
Ahmed Ali <ahmedali@Ahmeds-MacBook-Pro-2.local>
2021-01-10 10:48:38 +0200

    Initial commit

---
+++ FlickrApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrApp
+//
+//  Created by Ahmed Ali on 10/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
